86a98e4ccc86ee008a643154b744017730bf4324
Merry Damsko <m.d4msk0@gmail.com>
2022-10-29 04:13:46 +0200

    potmeter functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,15 @@
 void setup() {
   Serial.begin(9600);
 }
+int null = -1;
+int potValue, potValPrev = null;
 
-void loop() {
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
-  delay(1);
+void loop() {                 
+  potValue = analogRead(A0);
+  int value = map(potValue, 0, 1023, 0, 255);
+  if (value != potValPrev && value % 5 == 0)
+  {
+    potValPrev = value;
+    Serial.println(value);
+  }
 }
