906867d79a8ec6c5b6b10b333c9b003334a70659
Merry Damsko <m.d4msk0@gmail.com>
2022-10-29 04:42:13 +0200

    potmeter in function

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,19 @@
 void setup() {
   Serial.begin(9600);
 }
-int null = -1;
-int potValue, potValPrev = null;
+
+//Prototype
+void readPot();
 
 void loop() {                 
+  readPot();
+}
+
+//Function
+void readPot(){
+  int null = -1;
+  static int potValue, potValPrev = null;
+
   potValue = analogRead(A0);
   int value = map(potValue, 0, 1023, 0, 255);
   if (value != potValPrev && value % 5 == 0)
